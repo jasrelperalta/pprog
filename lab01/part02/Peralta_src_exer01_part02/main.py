929f219161f134ee5752eda4037500d513eb53ce
jasrelperalta <jtperalta1@up.edu.ph>
2023-03-13 00:40:55 +0800

    chore: edited file structure

---
+++ lab01/part02/Peralta_src_exer01_part02/main.py
@@ -0,0 +1,123 @@
+import numpy as np
+import random
+import datetime
+
+# prettier printing options
+np.set_printoptions(formatter={'float': '{: 0.5f}'.format})
+
+# initialize data
+n = int(input("enter size: "))
+n += 1
+dist = 10
+
+# create a zero nxn matrix
+mat = np.zeros((n,n), dtype = float)
+
+# randomize elevation values for gridpoints divisible by 10
+for i in range(n):
+    for j in range(n):
+        if i % dist == 0 and j % dist == 0:
+            mat[i][j] = random.uniform(0.0, 1000.0)
+
+# for given example in exer file
+# mat[0][0] = 200
+# mat[0][10] = 250
+# mat[10][0] = 280
+# mat[10][10] = 300
+
+# interpolate function
+def terrain_inter(mat):
+    for i in range(0,n):
+        for j in range(0,n):
+            if mat[i][j] != 0:
+                continue
+            if (i % dist == 0):
+                get_row_val(i,j)
+    for i in range(0,n):
+        for j in range(0,n):
+            if (mat[i][j] == 0):
+                get_col_val(i,j)
+    print("\n")
+
+
+# dp array format:
+# dp = [[x1,y1][x2,y2]]
+
+# interpolate rows with random values
+def get_row_val(i,j):
+    dp = get_datapoints_row(i,j)
+    x = j               # j -> row
+    x1 = dp[0][0]
+    x2 = dp[1][0]
+    y1 = dp[0][1]
+    y2 = dp[1][1]
+    res = fcc(x1,y1,x2,y2,x)
+    mat[i][j] = res
+
+# interpolate columns
+def get_col_val(i,j):
+    dp = get_datapoints_col(i,j)
+    # dp = [[x1,y1][x2,y2]]
+    x = i               # i -> row
+    x1 = dp[0][0]
+    x2 = dp[1][0]
+    y1 = dp[0][1]
+    y2 = dp[1][1]
+    res = fcc(x1,y1,x2,y2,x)
+    mat[i][j] = res
+
+
+# get closest datapoints to the current gridpoint
+def get_datapoints_row(i,j):
+    dp = []
+    dp.append(get_nearest_row(i,j,-1))
+    dp.append(get_nearest_row(i,j,+1))
+    return dp
+def get_datapoints_col(i,j):
+    dp = []
+    dp.append(get_nearest_col(i,j,-1))
+    dp.append(get_nearest_col(i,j,+1))
+    return dp
+
+# x, y -> point; dir -> direction 
+# change direction to check to the nearest 10
+## improved from recursion from previous exercise to direct computation
+def get_nearest_row(i,j,dir):
+    # go up
+    if dir < 0:
+        dir = j - (j % 10)
+    # go down
+    else:
+        dir = j + (10 - (j % 10))
+    return [dir,mat[i][dir]]
+
+def get_nearest_col(i,j,dir):
+    # go left
+    if dir < 0:
+        dir = i - (i % 10)
+    # go right
+    else:
+        dir = i + (10 - (i % 10))
+    return [dir,mat[dir][j]]
+
+# follow given FCC formula
+def fcc(x1,y1,x2,y2,x):
+    return (y1 + (((x-x1)/(x2-x1)) * (y2-y1)))
+
+# print initial matrix
+print(mat)
+
+# record time before interpolation
+time_before = datetime.datetime.now()
+
+# interpolate matrix
+terrain_inter(mat)
+
+# record time after interpolation
+time_after = datetime.datetime.now()
+
+# print interpolation time
+print(time_after-time_before)
+
+# print resulting matrix
+print(mat)
